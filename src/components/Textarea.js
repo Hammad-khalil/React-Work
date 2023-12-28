6c4076439c2baa38a1cb4ab53d1d2fd025711fe5
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-28 23:58:14 +0500

    MAke a Text convertor using State and Event Handlers

--- src/components/Textarea.js
+++ src/components/Textarea.js
@@ -2,18 +2,19 @@ import React, {useState} from "react"
 
 export default function Textarea(props) {
     const handleUpClick = () =>{
-        console.log("Uppercase Clicked");
-        setText("Done !!!")
+        // console.log("Uppercase Clicked");
+        setText(text.toUpperCase().trim());
     }
-    const handleOnChange = () =>{
-        console.log("Function Onchange !!");
+    const handleOnChange = (event) =>{
+        setText(event.target.value);
+        // console.log("Function Onchange !!");
     }
     const [text,setText] = useState("Enter Text Here");
     return (
     <div>
         <h1>{props.heading}</h1>
   <div className="form-group">
-    <textarea className="form-control" onChange={handleOnChange} placeholder={text} id="exampleFormControlTextarea1" rows="3"></textarea>
+    <textarea className="form-control" onChange={handleOnChange} value={text} id="exampleFormControlTextarea1" rows="3"></textarea>
   </div>
   <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
     </div>
