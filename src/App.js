c61bb126043062858851510b7c4e73e516a51337
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-28 20:22:59 +0500

    Used Props for Providing data to component from parent file

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
 {/* NavBar */}
 
 
-<Navbar/>
+<Navbar title="Twitter" menu="Blogs"/>
 </>
 
 
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
 
-export default function Navbar() {
+export default function Navbar(props) {
   return (
    
-<nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="/">TextUtils</a>
+<nav className="navbar navbar-expand-lg navbar-light bg-danger">
+  <a className="navbar-brand" href="/">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -13,6 +13,9 @@ export default function Navbar() {
     <ul className="navbar-nav mr-auto">
       <li className="nav-item active">
         <a className="nav-link" href="/">Home <span className="sr-only">(current)</span></a>
+      </li>
+      <li className="nav-item">
+        <a className="nav-link" href="/">{props.menu}</a>
       </li>
       <li className="nav-item">
         <a className="nav-link" href="/">About</a>
