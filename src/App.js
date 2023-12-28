b1225a7253acb9e72b296bf111b3a34a0951cbcb
Hammad-khalil <shhamadkhalil0786@gmail.com>
2023-12-28 22:12:07 +0500

    Will be using useState to convert text to uppercase

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import Textarea from './components/Textarea';
 
 function App() {
   return (
@@ -10,6 +11,10 @@ function App() {
 
 {/* <Navbar title="React JS" menu="Blogs"/> */}
 <Navbar/>
+
+<div className="container">
+<Textarea heading="Any AnyText here to Convert into Uppercase"/>
+</div>
 </>
 
 
---
+++ src/components/Textarea.js
@@ -0,0 +1,20 @@
+import React, {useState} from "react"
+
+export default function Textarea(props) {
+    const handleUpClick = () =>{
+        console.log("Uppercase Clicked");
+    }
+    const handleOnChange = () =>{
+        console.log("Function Onchange !!");
+    }
+    const [text,setText] = useState("Enter Text Here");
+    return (
+    <div>
+        <h1>{props.heading}</h1>
+  <div className="form-group">
+    <textarea className="form-control" onChange={handleOnChange} placeholder={text} id="exampleFormControlTextarea1" rows="3"></textarea>
+  </div>
+  <button className="btn btn-primary" onClick={handleUpClick}>Convert To Uppercase</button>
+    </div>
+  )
+}
